5bb6e9920070b149fa0da2c19211e2eef5eb9b14
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-06-25 21:44:19 +0300

    Add files via upload

--- config.lua
+++ config.lua
@@ -3,5 +3,5 @@ Config = {}
 Config.SpeedLimit = 120 -- Hız 120 ve üstünde ise kırmızı renkte görünür.
 Config.FuelLimit = 20 -- Benzin 20 ve altında ise kırmızı renkte yanıp söner.
 Config.CruiseInput = 29 -- Hız Sabitleyiciyi aktif eden tuş.
-Config.SeatBeltVolume = 0.5 -- Emniyet Kemerinin sesi.
+Config.SeatBeltVolume = 0.5 -- Emniyet Kemerinin sesi. [Max:1.0, Min:0.0]
 Config.SeatBeltInput = 311 -- Emniyet Kemerini açıp/kapatan tuş.
